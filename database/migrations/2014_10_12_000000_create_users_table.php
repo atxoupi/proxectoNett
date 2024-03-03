ec8f617ad491bfb027d6d646766fc405a9663893
Andres <andres.hermelo@gmail.com>
2024-03-03 18:08:01 +0100

    Creating tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,51 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('admin')->default(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        Schema::create('Principal', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('entradilla');
+            $table->string('cuerpo');
+            $table->string('imagen');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        Schema::create('SobreMi', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('texto');
+            $table->string('imagen');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        Schema::create('Servicios', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('entradilla');
+            $table->string('resumen');
+            $table->string('imagen');
+            $table->number('seccion');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('Eventos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->date('fecha');
+            $table->string('resumen');
+            $table->string('imagen');
+            $table->rememberToken();
+            $table->timestamps();
+        });                                                     
+
+        Schema::create('Contacto', function (Blueprint $table) {
+            $table->id();
+            $table->string('direccion');
             $table->rememberToken();
             $table->timestamps();
         });
